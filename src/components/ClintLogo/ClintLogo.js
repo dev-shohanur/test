a3d005537529d8a3422d01c9708b1070090d1212
MD Shohanur Rahman <98300635+dvsrshohan@users.noreply.github.com>
2023-05-06 04:26:17 +0600

    Add New Collection

--- src/components/ClintLogo/ClintLogo.js
+++ src/components/ClintLogo/ClintLogo.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-const ClintLogo = () => {
+const ClintLogo = ({ data }) => {
+  console.log(data.logoSection.nodes[0].frontmatter);
   return (
     <div className='container my-[84px]'>
       <div className="grid lg:grid-cols-5 md:grid-cols-4 grid-cols-2">
-        <img className='lg:mb-0 mb-4' src="/images/logo1.png" alt="" srcset="" />
-        <img className='lg:mb-0 mb-4' src="/images/logo2.png" alt="" srcset="" />
-        <img className='lg:mb-0 mb-4' src="/images/logo3.png" alt="" srcset="" />
-        <img className='lg:mb-0 mb-4' src="/images/logo4.png" alt="" srcset="" />
-        <img className='lg:mb-0 mb-4' src="/images/logo5.png" alt="" srcset="" />
+        {data.logoSection.nodes[0].frontmatter.Logos.map(clintLogo => <img className='lg:mb-0 mb-4' src={clintLogo.logo} alt="" srcset="" />)}
       </div>
     </div>
   );
