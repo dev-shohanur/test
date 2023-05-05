217a9a072e07c4de3d80aae454babe112df95d5c
MD Shohanur Rahman <98300635+dvsrshohan@users.noreply.github.com>
2023-05-06 04:36:04 +0600

    Add New Collection

--- src/components/ClintLogo/ClintLogo.js
+++ src/components/ClintLogo/ClintLogo.js
@@ -4,7 +4,7 @@ const ClintLogo = ({ data }) => {
   return (
     <div className='container my-[84px]'>
       <div className="grid lg:grid-cols-5 md:grid-cols-4 grid-cols-2">
-        {data.logoSection.nodes[0].frontmatter.Logos.map(clintLogo => <img className='lg:mb-0 mb-4' src={clintLogo.logo} alt="" srcset="" />)}
+        {/* {data.logoSection.nodes[0].frontmatter.Logos.map(clintLogo => <img className='lg:mb-0 mb-4' src={clintLogo.logo} alt="" srcset="" />)} */}
       </div>
     </div>
   );
